a30686c1e2fd30ec8fc71c39a3e215de1cdf7073
Austin Fell <austinrf2918@yahoo.com>
2016-03-29 03:49:26 -0400

    Added my ACTUAL stuff

---
+++ js/navCherries.js
@@ -0,0 +1,22 @@
+$(document).ready(function()
+{
+    $('#navigationDataOne').mouseover(function(){
+        $('#navigationDataTwo').addClass("activeDataNonHover");
+    });
+
+    $('#navigationDataOne').mouseout(function(){
+        $('#navigationDataTwo').removeClass("activeDataNonHover");
+    });
+
+    $('#navigationDataTwo').mouseover(function(){
+    });
+
+    $('#navigationDataThree').mouseover(function(){
+        $('#navigationDataTwo').addClass("activeDataNonHover");
+    });
+
+    $('#navigationDataThree').mouseout(function(){
+        $('#navigationDataTwo').removeClass("activeDataNonHover");
+    });
+
+});
